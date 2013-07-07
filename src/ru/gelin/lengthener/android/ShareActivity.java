2e1b4aef37821ad6339241202fd3043db649246b
Denis Nelubin <dnelubin@gmail.com>
2013-07-07 20:39:29 +0700

    Implemented activity as progress dialog. URLs processing as AsyncTask.

--- src/ru/gelin/lengthener/android/ShareActivity.java
+++ src/ru/gelin/lengthener/android/ShareActivity.java
@@ -2,6 +2,7 @@ package ru.gelin.lengthener.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import ru.gelin.lengthener.TextUrlLengthener;
 
@@ -14,6 +15,7 @@ public class ShareActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.progress);
 
         Intent intent = getIntent();
         if (!Intent.ACTION_SEND.equals(intent.getAction())) {
@@ -31,13 +33,24 @@ public class ShareActivity extends Activity {
             return;
         }
 
-        String newText = TextUrlLengthener.lengthenUrls(text);
+        new LengthenTask().execute(text);
+    }
 
-        Intent newIntent = new Intent(intent);
-        newIntent.putExtra(Intent.EXTRA_TEXT, newText);
-        startActivity(Intent.createChooser(newIntent, getString(R.string.lengthened_urls)));
+    class LengthenTask extends AsyncTask<String, Void, String> {
 
-        finish();
+        @Override
+        protected String doInBackground(String... strings) {
+            return TextUrlLengthener.lengthenUrls(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String text) {
+            Intent newIntent = new Intent(getIntent());
+            newIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(Intent.createChooser(newIntent, getString(R.string.lengthened_urls)));
+            finish();
+        }
+
     }
 
 }
