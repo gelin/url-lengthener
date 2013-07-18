0cc1f1922e307421e37ffdf5763570ad324311ca
Denis Nelubin <dnelubin@gmail.com>
2013-07-19 00:17:12 +0700

    Bulding settings screen.

---
+++ src/ru/gelin/lengthener/android/SettingsActivity.java
@@ -0,0 +1,14 @@
+package ru.gelin.lengthener.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *  Activity with settings.
+ */
+public class SettingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
