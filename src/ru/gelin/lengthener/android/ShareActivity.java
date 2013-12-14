13df8a81eb6200239342368dfa0512035d1d91d7
Denis Nelubin <dnelubin@gmail.com>
2013-12-14 16:52:02 +0700

    Added network detection.

--- src/ru/gelin/lengthener/android/ShareActivity.java
+++ src/ru/gelin/lengthener/android/ShareActivity.java
@@ -2,8 +2,11 @@ package ru.gelin.lengthener.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 import ru.gelin.lengthener.TextUrlLengthener;
 
 /**
@@ -26,6 +29,11 @@ public class ShareActivity extends Activity {
             finish();
             return;
         }
+        if (!isNetworkAvailable()) {
+            passThrough(intent);
+            finish();
+            return;
+        }
 
         String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
         if (text == null) {
@@ -34,6 +42,28 @@ public class ShareActivity extends Activity {
         }
 
         new LengthenTask().execute(text);
+    }
+
+    /**
+     *  Check availability of network connections.
+     *  Returns true if any network connection is available.
+     */
+    boolean isNetworkAvailable() {
+        ConnectivityManager manager =
+                (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        if (info == null) {
+            return false;
+        }
+        return info.isAvailable();
+    }
+
+    /**
+     *  Pass the original intent through without modification.
+     */
+    void passThrough(Intent intent) {
+        Toast.makeText(this, R.string.cannot_lengthen, Toast.LENGTH_LONG).show();
+        startActivity(Intent.createChooser(intent, getString(R.string.not_lengthened_urls)));
     }
 
     class LengthenTask extends AsyncTask<String, Void, String> {
@@ -50,6 +80,7 @@ public class ShareActivity extends Activity {
             startActivity(Intent.createChooser(newIntent, getString(R.string.lengthened_urls)));
             finish();
         }
+
 
     }
 
