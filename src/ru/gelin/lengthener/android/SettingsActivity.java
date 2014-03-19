abf729953e648ed4167e543011705cc29b836d3d
Denis Nelubin <dnelubin@gmail.com>
2014-03-20 00:51:40 +0700

    Building tablet navigation.

--- src/ru/gelin/lengthener/android/SettingsActivity.java
+++ src/ru/gelin/lengthener/android/SettingsActivity.java
@@ -10,12 +10,6 @@ public class SettingsActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //TODO: tablet support
-//        getFragmentManager().beginTransaction()
-//                .replace(android.R.id.content,
-//                        SettingsFragment.newInstance())
-//                .commit();
         setContentView(R.layout.settings_activity);
     }
 
--- src/ru/gelin/lengthener/android/SettingsFragment.java
+++ src/ru/gelin/lengthener/android/SettingsFragment.java
@@ -1,7 +1,10 @@
 package ru.gelin.lengthener.android;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -12,8 +15,33 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        String key = preference.getKey();
+        if ("help".equals(key)) {
+            showFragment(HelpFragment.newInstance());
+            return true;
+        } else if ("remove_query_domains".equals(key)) {
+            showFragment(DomainsListFragment.newInstance(AndroidLengthenerSettings.REMOVE_QUERY_DOMAINS_PREFIX));
+            return true;
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+    }
+
+    void showFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.settings_details, fragment)
+                .commit();
     }
 
 }
